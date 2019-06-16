0ea0917d640bbc0435d935fb52d8b547f6400754
Olja Ivkovic <olja.ivkovic97@gmail.com>
2019-06-16 17:51:33 +0200

    added additional validation for dtos

---
+++ MyComicList.DataAccess/Migrations/20190616150614_AddedColumnOriginPublishers.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyComicList.DataAccess.Migrations
+{
+    public partial class AddedColumnOriginPublishers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Origin",
+                table: "Publishers",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Origin",
+                table: "Publishers");
+        }
+    }
+}
